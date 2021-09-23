e814f5c8725faf3b812200ff9135ab5813fb185b
Dasho4ek <pdariya@gmail.com>
2021-09-22 20:23:17 -0400

    changes done

---
+++ src/test/java/Class1/Classwork.java
@@ -0,0 +1,19 @@
+package Class1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Classwork {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+
+        WebDriver driver= new ChromeDriver();
+
+
+
+
+
+    }
+}
+
--- src/test/java/Homework/Homework1.java
+++ src/test/java/Homework/Homework1.java
@@ -1,8 +1,21 @@
 package Homework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Homework1 {
     public static void main(String[] args) {
         System.out.println("Homework 1");
+
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.get ("https://www.facebook.com/");
+
+
+
+
     }
 
 
